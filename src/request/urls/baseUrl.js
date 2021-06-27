19f775f718e3910015d1ce0d85ae7839c2d309db
Jarrett <291979254@qq.com>
2021-06-27 19:09:06 +0800

    修改basebase

--- src/request/urls/baseUrl.js
+++ src/request/urls/baseUrl.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://192.168.0.103:8081'
+const BASE_URL = 'http://10.0.6.135:8081'
 // 
-// BASE_URL = "http://129.211.165.193:8081"
+// const BASE_URL = "http://129.211.165.193:8081"
 
 exports.BASE_URL = BASE_URL;
